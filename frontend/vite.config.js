ab35ffb6a1b62c77585398aa3397f1fce8059f9b
elenikose <elenikose7@gmail.com>
2026-06-14 13:16:38 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    // Uyarıyı susturmak yerine limitleri esnetiyoruz
+    chunkSizeWarningLimit: 1000, 
     rollupOptions: {
       output: {
+        // Büyük kütüphaneleri manuel olarak parçalara ayır
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return 'vendor';
